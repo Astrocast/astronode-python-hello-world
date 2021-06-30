f6f9811c02b37821dbb49b845574c4cc057daada
Gildas Seimbille <gseimbille@astrocast.com>
2021-06-30 16:18:19 +0200

    Various adjustements

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """
 
-File:               serial_send_receive.py
+File:               main.py
 Author:             Gildas Seimbille
 E-mail:             gseimbille@astrocast.com
 
@@ -10,38 +10,40 @@ Supported Hardware: Astronode S DevKit
 
 """
 
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 
 import binascii
 import codecs
 import crcmod
-import datetime
 import serial
 from binascii import hexlify
 from random import *
 
-SerialPort = 'COM7'
-#--------------------------------------------------------------------------------
+# To match with your local serial port
+SerialPort = 'COMx'
+
+# --------------------------------------------------------------------------------
 # Initializations
-#--------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
+
 ser = serial.Serial(
-    port = SerialPort,
-    baudrate = 9600,
-    bytesize = serial.EIGHTBITS,
-    parity = serial.PARITY_NONE,
-    stopbits = serial.STOPBITS_ONE,
-    timeout = 3,
-    inter_byte_timeout = 0.001
+    port=SerialPort,
+    baudrate=9600,
+    bytesize=serial.EIGHTBITS,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    timeout=3,
+    inter_byte_timeout=0.001
 )
 
 crc16 = crcmod.mkCrcFun(0x11021, rev=False, initCrc=0xffff, xorOut=0x0000)
 
 
-#--------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
 # Operation codes
-#--------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
+
 # main family
-DBG    = "01"
 PLD_ER = "25"
 PLD_DR = "26"
 PLD_FR = "27"
@@ -52,15 +54,14 @@ CFG_RR = "15"
 EVT_RR = "65"
 SAK_RR = "45"
 SAK_CR = "46"
-# debug sub-family
-DBG_SET_RTC    = "01"
-DBG_FAKE_ALARM = "03"
 
-#--------------------------------------------------------------------------------
+
+# --------------------------------------------------------------------------------
 # Payload, Geolocation and Wi-Fi configuration
 #
 # Here you can declare you data.
-#--------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
+
 payload = b"Hello world"
 latitude = 46.534363896181624
 longitude = 6.578710272772917
@@ -69,13 +70,14 @@ ssid = b"YOUR_WIFI_SSID"
 password = b"YOUR_WIFI_PASSWORD"
 token = b"YOUR_ASTROCAST_TOKEN"
 
-configuration_wifi = binascii.hexlify(ssid).ljust(66, b'0') + binascii.hexlify(password).ljust(128, b'0') + binascii.hexlify(token).ljust(194, b'0')
+configuration_wifi = binascii.hexlify(ssid).ljust(66, b'0') + \
+                     binascii.hexlify(password).ljust(128, b'0') + binascii.hexlify(token).ljust(194, b'0')
 configuration_wifi = configuration_wifi.decode("utf-8")
 
 
-#--------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
 # Function definitions
-#--------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
 
 def generate_geolocation(lat, lng):
     lat_tmp = '{:08x}'.format(int(lat * 1e7))
@@ -98,6 +100,7 @@ def generate_geolocation(lat, lng):
     geolocation += lng_tmp[1]
     return geolocation
 
+
 def generate_crc(data):
     crc_tmp = '{:04x}'.format(crc16(codecs.decode(data, 'hex')))
     crc = crc_tmp[2]
@@ -105,6 +108,7 @@ def generate_crc(data):
     crc += crc_tmp[0]
     crc += crc_tmp[1]
     return crc
+
 
 def send(opcode, data):
     msg = opcode
@@ -120,18 +124,21 @@ def send(opcode, data):
     print("[sent]      -->  " + " ".join(["{:02x}".format(x) for x in msg]))
     receive()
 
+
 def receive():
     output = ser.read(160)
     print("[received]  <--  " + " ".join(["{:02x}".format(x) for x in output]))
 
+
 def text_to_hex(text):
     return binascii.hexlify(text).decode('ascii')
+
 
 def generate_message(payload):
     return str(randint(0, 9999)) + text_to_hex(payload)
 
 
-#--------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
 # Send and receive on UART
 #
 # Here you can call the send function with the command and data you want.
@@ -140,13 +147,12 @@ def generate_message(payload):
 # it can be DEBUG to send command like setting the RTC date and time.
 # SUB-OPCODE is needed only for the DEBUG operation code.
 # DATA is the data related to the operation code and sub-operation code you use.
-#--------------------------------------------------------------------------------
-
+# --------------------------------------------------------------------------------
 
 # If using the Astronode S DevKit Wi-Fi (comment if using Satellite board)
 send(WIF_WR, configuration_wifi)
 
-# If you want to specify geolocation
+# If you want to specify geolocation (comment if not)
 send(GEO_WR, generate_geolocation(latitude, longitude))
 
 send(PLD_ER, generate_message(payload))
